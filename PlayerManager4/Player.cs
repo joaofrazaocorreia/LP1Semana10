9f3ac0d85cd87fc510d1b30c5a7212e0c47e8f0c
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-05-17 22:46:14 +0100

    Create project PlayerManager4

---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,21 @@
+using System;
+namespace PlayerManager4
+{
+    public class Player : IComparable<Player>
+    {
+        public string Name { get; }
+        public int Score { get; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public int CompareTo(Player other)
+        {
+            if (other == null) return 1;
+            return other.Score - this.Score;
+        }
+    }
+}
