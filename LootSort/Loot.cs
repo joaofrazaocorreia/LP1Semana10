dd24563c994484dd68545f1400868fbde71fbaca
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-05-19 13:19:21 +0100

    Add CompareTo to class Loot

--- LootSort/Loot.cs
+++ LootSort/Loot.cs
@@ -1,40 +1,49 @@
+using System;
+using System.Collections.Generic;
+
 namespace LootSort
 {
     /// <summary>
     /// The Loot class should implement IComparable<Loot>
     /// and override GetHashCode() and Equals()
     /// </summary>
-    public class Loot
+    public class Loot : IComparable<Loot>
+{
+    /// <summary>Type of loot.</summary>
+    public LootType Kind { get; }
+
+    /// <summary>Loot description.</summary>
+    public string Description { get; }
+
+    /// <summary>Loot value.</summary>
+    public float Value { get; }
+
+    /// <summary>
+    /// Create a new instance of loot.
+    /// </summary>
+    /// <param name="kind">Type of loot.</param>
+    /// <param name="description">Loot description.</param>
+    /// <param name="value">Loot value.</param>
+    public Loot(LootType kind, string description, float value)
     {
-        /// <summary>Type of loot.</summary>
-        public LootType Kind { get; }
-
-        /// <summary>Loot description.</summary>
-        public string Description { get; }
-
-        /// <summary>Loot value.</summary>
-        public float Value { get; }
-
-        /// <summary>
-        /// Create a new instance of loot.
-        /// </summary>
-        /// <param name="kind">Type of loot.</param>
-        /// <param name="description">Loot description.</param>
-        /// <param name="value">Loot value.</param>
-        public Loot(LootType kind, string description, float value)
-        {
-            Kind = kind;
-            Description = description;
-            Value = value;
-        }
-
-        /// <summary>
-        /// Return a nicely formatted string representing the loot instance.
-        /// </summary>
-        /// <returns>
-        /// A nicely formatted string representing the loot instance.
-        /// </returns>
-        public override string ToString() =>
-            $"[{Kind,15}]\t{Value:f2}\t{Description}";
+        Kind = kind;
+        Description = description;
+        Value = value;
     }
+
+    /// <summary>
+    /// Return a nicely formatted string representing the loot instance.
+    /// </summary>
+    /// <returns>
+    /// A nicely formatted string representing the loot instance.
+    /// </returns>
+    public override string ToString() =>
+        $"[{Kind,15}]\t{Value:f2}\t{Description}";
+
+    public int CompareTo(Loot other)
+        {
+            if (other == null) return 1;
+                return other.Kind - this.Kind;
+        }
+}
 }
